d55dd9a1778782b629c12e211547e095437ac5a2
ovaqvado <likeriklohhg@gmail.com>
2024-02-14 17:03:50 +0300

    hallo

---
+++ script.js
@@ -0,0 +1,7 @@
+// const button = document.querySelector('.btn_no');
+
+// button.addEventListener('mouseover', () => {
+//     const x = Math.random() * (window.innerWidth - button.clientWidth);
+//     const y = Math.random() * (window.innerHeight - button.clientHeight);
+//     button.style.transform = `translate(${y}px, ${x}px)`;
+// });
